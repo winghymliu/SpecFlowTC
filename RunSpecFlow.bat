4cd03d64510e031a443c0b71649a85b2ea10225a
wing liu <winghymliu@gmail.com>
2014-05-27 14:40:26 +0100

    first commit

---
+++ RunSpecFlow.bat
@@ -0,0 +1,3 @@
+"C:\Program Files (x86)\NUnit 2.6.3\bin\nunit-console.exe" /labels /out=TestResult.txt /xml=TestResult.xml /framework=net-4.0 SpecFlowTC.Spec\bin\Debug\SpecFlowTC.Spec.dll
+ 
+"C:\Workspace\SpecFlowTC\SepcFlowTC\packages\SpecFlow.1.9.0\tools\specflow.exe" nunitexecutionreport SpecFlowTC.Spec\SpecFlowTC.Spec.csproj
---
+++ SpecFlowTC.Spec/TestSteps.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using NUnit.Framework;
+using TechTalk.SpecFlow;
+
+namespace SpecFlowTC.Spec
+{
+    [Binding]
+    public class TestSteps
+    {
+        public List<int> Inputs = new List<int>();
+        public int Result = 0;
+
+        [Given(@"I have entered (.*) into the calculator")]
+        public void GivenIHaveEnteredIntoTheCalculator(int p0)
+        {
+            Inputs.Add(p0);
+        }
+        
+        [When(@"I press add")]
+        public void WhenIPressAdd()
+        {
+            Result = Inputs.Aggregate(0, (current, input) => current + input);
+        }
+        
+        [Then(@"the result should be (.*) on the screen")]
+        public void ThenTheResultShouldBeOnTheScreen(int p0)
+        {
+            Assert.AreEqual(p0, Result);
+        }
+    }
+}
